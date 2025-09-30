c407b577205fb154af9f81b22e2a31da664c8e1e
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-30 11:53:27 -0300

    first commit

---
+++ src/main/java/com/totex/PirateRegister/PirateRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.totex.PirateRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirateRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PirateRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totex/PirateRegister/PirateRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totex.PirateRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PirateRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
