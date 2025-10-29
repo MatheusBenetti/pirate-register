554e42a455ebc59d3923f798d360e64f55f41f93
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-29 09:52:15 -0300

    add get all crews to controller

--- src/main/java/com/totex/PirateRegister/Crew/controller/CrewController.java
+++ src/main/java/com/totex/PirateRegister/Crew/controller/CrewController.java
@@ -1,5 +1,6 @@
 package com.totex.PirateRegister.Crew.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class CrewController {
 
+    @GetMapping("/get-crews")
+    public String getAllCrews() {
+        return "Return crews";
+    }
 }
